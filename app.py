d26708d9c4ec30b9f6a1beb4fd2eebcf5da24966
James <jameskipngetich166@gmail.com>
2024-06-26 15:06:25 +0300

    My first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+ 
+app=Flask(__name__)
+app.secret_key='someone is lazy'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact_me')
+def contact_me():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
